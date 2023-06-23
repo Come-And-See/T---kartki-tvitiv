21d4d237d2195557ffc51023b58d8d32710e9843
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-06-23 14:38:08 +0300

    add: initial files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/test-kartki-tvitiv/",
+  base: "/test-kartki-tvitiv/",
   plugins: [react()],
 });
