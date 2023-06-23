45c3f4659cbc5dd6f158882246d8611e9614ca73
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-06-23 15:34:31 +0300

    add: HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/test-kartki-tvitiv'>
+    <HashRouter basename='/test-kartki-tvitiv'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
