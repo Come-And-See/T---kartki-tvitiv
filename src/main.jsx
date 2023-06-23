3df26c7c1af3dfc810fce1b3d1f11ab023591208
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-06-23 15:38:06 +0300

    UP

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename='/test-kartki-tvitiv'>
+    <HashRouter basename='/test-kartki-tvitiv/'>
       <App />
     </HashRouter>
   </React.StrictMode>
