53dcf5210d5a0732b4070b764fd773fbf741fc25
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-06-23 16:45:07 +0300

    remove: last changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://come-and-see.github.io/test-kartki-tvitiv/",
+  base: "https://come-and-see.github.io/test-kartki-tvitiv",
   plugins: [react()],
 });
