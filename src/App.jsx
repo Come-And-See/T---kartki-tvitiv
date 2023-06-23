9eef33a679e1dfd8a0d48a21e74179fde06397c6
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-06-23 16:05:13 +0300

    UP - Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import './App.css'
-import Home from './Page/Home/Home'
-import { Navigate, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import Home from './Page/Home/Home';
+import { HashRouter, Link, Route, Switch } from 'react-router-dom';
 import NavLinks from './Components/NavLinks/NavLinks';
 import Tweets from './Page/Tweets/Tweets';
 
 function App() {
-
-
   return (
-    <>
+    <HashRouter>
       <nav>
         <NavLinks />
       </nav>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/tweets' element={<Tweets />} />
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
-      </Routes>
-    </>
-  )
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/tweets" component={Tweets} />
+        <Route path="*" component={Home} />
+      </Switch>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
