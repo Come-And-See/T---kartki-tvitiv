6b4eea3f4f24f9b6aa66c918adcc9ba014f4ae9b
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-06-23 16:14:57 +0300

    UP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './Page/Home/Home';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import NavLinks from './Components/NavLinks/NavLinks';
 import Tweets from './Page/Tweets/Tweets';
 
@@ -12,11 +12,11 @@ function App() {
         <NavLinks />
       </nav>
 
-      <Switch>
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route exact path="/tweets" component={Tweets} />
         <Route path="*" component={Home} />
-      </Switch>
+      </Routes>
     </HashRouter>
   );
 }
